2017da102697d55cfecff407f71e84aa10c53e93
SoumikMondal01 <soumikmondal663@gmail.com>
2022-12-24 21:54:13 +0530

    first commit

---
+++ public/client.js
@@ -0,0 +1,54 @@
+const socket = io()
+let name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+do {
+    name = prompt('Please enter your name: ')
+} 
+while(!name)
+
+// code of how the typed message will be sent
+
+textarea.addEventListener('keyup', (e) => { // this keyup means it will act in pressing any key
+    if(e.key === 'Enter') { // if the pressed is enter then
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // Append 
+    appendMessage(msg, 'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+    // Send to server 
+    socket.emit('message', msg)
+
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup  // the markup is stored in the mainDiv
+    messageArea.appendChild(mainDiv)    // the mainDiv textarea content will be shown in the messageArea
+}
+
+// Recieve messages 
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom()
+})
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
